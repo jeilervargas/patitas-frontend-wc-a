d9ed99b89384cebc7466d19bb243fb8896f12857
Jeiler Vargas <jeiler.a.vargas@gmail.com>
2024-10-22 23:32:36 -0500

    Se corrigió la interfaz AutenticacionCliente y se añadieron los DTO SignOutRequestDTO y SignOutResponseDTO. Además, se añadió la clase AutenticacionFeignConfig

--- src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/cliente/AutenticacionCliente.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc_a/cliente/AutenticacionCliente.java
@@ -4,8 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import pe.edu.cibertec.patitas_frontend_wc_a.config.AutenticacionFeignConfig;
+import pe.edu.cibertec.patitas_frontend_wc_a.dto.SignOutRequestDTO;
+import pe.edu.cibertec.patitas_frontend_wc_a.dto.SignOutResponseDTO;
 
-@FeignClient(name = "autenticacion", url = "http://localhost:8081/autenticacion", configuration = AutenticacionFeingConfig.class)
+@FeignClient(name = "autenticacion", url = "http://localhost:8081/autenticacion", configuration = AutenticacionFeignConfig.class)
 public interface AutenticacionCliente {
 
     @PostMapping("/signout")
